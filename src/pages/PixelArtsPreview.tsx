e9332a8d10ef208d7a88355e5e0239c47c786da7
kbc18a07 <kbc18a07@stu.kawahara.ac.jp>
2021-06-09 11:31:21 +0900

    fix:大した変更なし

--- src/pages/PixelArtsPreview.tsx
+++ src/pages/PixelArtsPreview.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-
+import { Button, ButtonGroup } from '@material-ui/core';
+import { BrowserRouter as Route, Switch, Link } from 'react-router-dom';
+ 
 const PixelArtsPreview = () => {
   return (
-    <div>プレビュー画面</div>
+    <React.Fragment>
+      <ButtonGroup orientation="vertical">
+        <Button component={Link} to='/pixel-arts-edit/:id'>編集</Button>
+        <Button>削除</Button>
+        <Button>再生</Button>
+      </ButtonGroup>
+    </React.Fragment>
   );
 }
 
