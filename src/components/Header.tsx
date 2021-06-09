4dadec7319e0e30368e0b4ae002d7c066f7f0ada
kbc18a09 <kbc18a09@stu.kawahara.ac.jp>
2021-06-09 10:43:30 +0900

    フォーマット整理

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,23 +9,23 @@ import { Link } from 'react-router-dom'
 
 
 const Header = () => {
- 
+
   return (
-    
+
     <div className="headermain">
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
-        </IconButton>
-        <Typography variant="h6" className="title">
-          <div className="title">ドットピカ</div>
-        </Typography>
-        <Button  color="inherit"><div className="button-top"><Link to="/pixel-arts-edit" style={{textDecoration:"none"}}>色を塗る</Link></div></Button>
-        <Button  color="inherit"><div className="button-top"><Link to="/pixel-arts-me" style={{textDecoration:"none"}}>一覧へ</Link></div></Button>
-        <Button  color="inherit"><div className="button-top"><Link to="/pixel-arts-templates" style={{textDecoration:"none"}}>テンプレートを選ぶ</Link></div></Button>
-      </Toolbar>
-    </AppBar>
-  </div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6" className="title">
+            <div className="title">ドットピカ</div>
+          </Typography>
+          <Button color="inherit"><div className="button-top"><Link to="/pixel-arts-edit" style={{ textDecoration: "none" }}>色を塗る</Link></div></Button>
+          <Button color="inherit"><div className="button-top"><Link to="/pixel-arts-me" style={{ textDecoration: "none" }}>一覧へ</Link></div></Button>
+          <Button color="inherit"><div className="button-top"><Link to="/pixel-arts-templates" style={{ textDecoration: "none" }}>テンプレートを選ぶ</Link></div></Button>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom'
 
 
 
-  const Index = () => {
-    　　
-      return (
-        
-        <div>
-          <h1>ドットピカ</h1>
-          <Link to="/pixel-arts-edit"style={{textDecoration:"none"}}><div className="link">色を塗る</div></Link>
-          <Link to="/pixel-arts-me"style={{textDecoration:"none"}}><div className="link">一覧ヘ</div></Link>
-          <Link to="/pixel-arts-templates"style={{textDecoration:"none"}}><div className="link">テンプレートで選ぶ</div></Link>
-        </div>
-      
-        
-           
-        );
-         }
+const Index = () => {
+
+  return (
+
+    <div>
+      <h1>ドットピカ</h1>
+      <Link to="/pixel-arts-edit" style={{ textDecoration: "none" }}><div className="link">色を塗る</div></Link>
+      <Link to="/pixel-arts-me" style={{ textDecoration: "none" }}><div className="link">一覧ヘ</div></Link>
+      <Link to="/pixel-arts-templates" style={{ textDecoration: "none" }}><div className="link">テンプレートで選ぶ</div></Link>
+    </div>
+
+
+
+  );
+}
 
 
 
