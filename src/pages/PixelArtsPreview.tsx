818a7ca8a34762e7061e424992cfc33dbe985bbb
kbc18a07 <kbc18a07@stu.kawahara.ac.jp>
2021-06-17 10:05:52 +0900

    fix:編集、削除、実行ボタンのアクション割り当て

--- src/pages/PixelArtsPreview.tsx
+++ src/pages/PixelArtsPreview.tsx
@@ -5,13 +5,36 @@ import { BrowserRouter as Route, Switch, Link } from 'react-router-dom';
 const PixelArtsPreview = () => {
   return (
     <React.Fragment>
+      <PreviewPixels/>
       <ButtonGroup orientation="vertical">
         <Button component={Link} to='/pixel-arts-edit/:id'>編集</Button>
-        <Button>削除</Button>
-        <Button>再生</Button>
+        <Button onClick={DeletePixels}>削除</Button>
+        <Button onClick={ReflectPixels}>実行</Button>
       </ButtonGroup>
     </React.Fragment>
   );
 }
 
 export default PixelArtsPreview;
+
+//プレビュー表示領域、仮のcss
+const divStyle = {
+  backgroundColor: 'red',
+  width: 500,
+  height: 500
+};
+
+//プレビュー表示
+const PreviewPixels = () => {
+  return <div style={divStyle}>プレビュー</div>;
+}
+
+//削除
+const DeletePixels = () => {
+  console.log("DeletePixels");
+}
+
+//実行
+const ReflectPixels = () => {
+  console.log("ReflectPixels");
+}
