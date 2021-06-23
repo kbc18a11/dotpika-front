22d399b39ea56edab848b133ed324dc375bf2dcb
kbc18a07 <kbc18a07@stu.kawahara.ac.jp>
2021-06-23 09:39:27 +0900

    masterブランチとマージすれば解決する

--- src/pages/PixelArtsPreview.tsx
+++ src/pages/PixelArtsPreview.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Route, Switch, Link } from 'react-router-dom';
 /*
 問題発生中!
 Buttonタグ中の文字列が縦になる
-BButtonGroupの下二つが影響を受ける
+実行、削除のボタンが影響を受けている
 
 ・発生源
 PixelArtsEdit.cssが影響している
@@ -15,7 +15,7 @@ const PixelArtsPreview = () => {
     <React.Fragment>
       <PreviewPixels/>
       <ButtonGroup orientation="vertical">
-        <Button component={Link} to='/pixel-arts-edit/:id'>編集</Button>
+      <Button component={Link} to='/pixel-arts-edit/:id'>編集</Button>
         <Button onClick={DeletePixels}>削除</Button>
         <Button onClick={ReflectPixels}>実行</Button>
       </ButtonGroup>
