d36c2241d1b5997550363541c12451d0e88775b5
kbc18a10 <kbc18a10@stu.kawahara.ac.jp>
2021-07-13 12:12:25 +0900

    編集画面のmaterial-UIとredo・undoボタンの追加

--- src/components/SavePixelArt.tsx
+++ src/components/SavePixelArt.tsx
@@ -1,23 +1,33 @@
-import React from "react";
-import Button from '@material-ui/core/Button';
-import Input from "@material-ui/core/Input";
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
+import {
+    Button,
+    makeStyles,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    FormControl,
+    FormLabel,
+    OutlinedInput,
+} from "@material-ui/core/";
 
 const useStyles = makeStyles({
-    saveName: {
+    standardError: {
       width:230,
-      height:30,
+      height:40,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       color: 'white',
       padding:0,
       float:'left',
-      marginTop:30,
+      marginTop:25,
     },
     saveButton:{
-      height:30,
+      height:50,
       width:50,
-      marginTop:30,
+      marginTop:25,
     },
     shineButton:{
         height:60,
@@ -39,11 +49,38 @@ type SavePixelArtProps = {
 }
 
 const SavePixelArt = (props: SavePixelArtProps) => {
-    const classes = useStyles();
+    const eMsg:string = "12文字以内で名前を入力してください"
     const name = props.name;
+    const [open, setOpen] = useState(false);
+    const [err,setErr] = useState(true);
+    const [errMsg,setErrMsg] = useState(eMsg);
+    const classes = useStyles();
+    if(err){
+        if(0<name.length&&name.length<13){
+            setErr(false);
+            setErrMsg('OK');
+        }
+    }
+
+    const handleClickOpen = () => {
+        if(!err){
+            setOpen(true);
+        }
+    };
+    
+    const handleCloseYes = () => {
+        setOpen(false);
+        handlePixelArtSaveW();
+    };
+
+    const handleCloseNo = () => {
+        setOpen(false);
+    };
 
     const handleNameChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-        props.onNameChange(e.target.value);
+        const name = e.target.value;
+        judgeName(name);
+        props.onNameChange(name);
     }
 
     const handlePixelArtSaveW = () => {
@@ -58,23 +95,59 @@ const SavePixelArt = (props: SavePixelArtProps) => {
         props.onMakeItShine();
     }
 
+    const judgeName = (name:string) => {
+        if(0 < name.length && name.length < 13){
+            setErr(false);
+            setErrMsg("OK");
+        }else{
+            setErr(true);
+            setErrMsg(eMsg);
+        }
+    }
+
     return(
         <div id="saveContent">
             <div id="savePixelArt">
-                <form className={classes.saveName} noValidate autoComplete="off" 
-                onKeyPress={e => {
-                    if (e.key === 'Enter') {
-                        handlePixelArtSaveW()
-                    }
-                }}>
-                    <Input placeholder="名前を付けて保存" value={name} onChange={handleNameChange} inputProps={{ 'aria-label': 'description' }}/>
-                </form>
-                <Button className={classes.saveButton} variant="contained" onClick={handlePixelArtSaveW}>保存</Button> 
+                <FormControl error={err} disabled={false}>
+                    <FormLabel>
+                        <div　id="errorMessage">{errMsg}</div>
+                        <OutlinedInput
+                            fullWidth
+                            id="inputName"
+                            name="inputName"
+                            type="text"
+                            placeholder="名前を付けて保存"
+                            required={true}
+                            onChange={handleNameChange} 
+                            value={name}
+                        />
+                    </FormLabel>
+                </FormControl>
+                <Button className={classes.saveButton} variant="contained" onClick={handleClickOpen}>保存</Button>
             </div>
             <div id="shineDeleteButtons">
                 <Button className={classes.shineButton} variant="contained" onClick={handleMakeItShine}>光らせる</Button> 
                 <Button className={classes.deleteButton} variant="contained" onClick={handleDeleteColor}>全消し</Button> 
             </div>
+            <Dialog
+            open={open}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description">
+                <DialogTitle id="alert-dialog-title">{}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        「{name}」を保存しますか？  
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseYes} color="primary">
+                    <Link to="/pixel-arts-me" id="toLink">はい</Link>
+                    </Button>
+                    <Button onClick={handleCloseNo} color="primary" autoFocus>
+                        いいえ
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
